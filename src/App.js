f6d42d4ead9414fbc911c8807c192e9ffbdc809b
Derek Baumgartner <Derek.Baumgartner@gmail.com>
2019-11-09 15:24:15 -0700

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import db from './img/db.svg';
+import hero from './img/hero.png';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <header>
+            <img src={db} className="site-logo" alt="logo" />
+            <nav>
+              <ul>
+                <li>
+                  <NavLink className="home-link" exact to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink className="work-link" to="/work">Work</NavLink>
+                </li>
+                <li>
+                  <NavLink className="resume-link" to="/resume">Resume</NavLink>
+                </li>
+                <li>
+                  <NavLink className="contact-link" to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <Switch>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+            <Route path="/work">
+              <Work />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+function Home() {
+  return <div>
+    <img src={hero} className="hero" alt="hero image" />
+
+    Hello
+  </div>
+}
+
+function Resume() {
+  return <h2>Resume</h2>
+}
+
+function Contact() {
+  return <h2>Contact</h2>
+}
+
+function Work() {
+  return <h2>Work</h2>
+}
+
+export default App
